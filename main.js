98cd029bfe04d28839d93e8c2aecd93c1cd324b5
tmorozov <radist@desant.biz>
2013-08-15 11:26:36 +0300

    oprimized

--- main.js
+++ main.js
@@ -10,15 +10,49 @@ function clone(fn) {
     return fn.bind({});
 };
 
+function findIndex(arr, val) {
+  // var doLog = val === 2147218497;
+
+  if(! arr.length) {
+    return -1;
+  }
+
+  var start = 0;
+  var end = arr.length - 1;
+
+  if(val >= arr[end]) {
+    return end;
+  }
+  if(val <= arr[start]) {
+    return start;
+  }
+
+  while (start != end ) {
+    var mid = ~~((start + end) / 2);
+// if (doLog) {
+//   console.log('indexes', start, mid, end);
+//   console.log('values', arr[start], arr[mid], arr[end]);
+// }
+    if(arr[mid] > val) {
+      end = mid;
+      if( end > start ) {
+        end--;
+      }
+    } else if (arr[mid] < val) {
+      start = mid;
+      if( end > start) {
+        start++;
+      }
+    } else {
+      return mid;
+    }
+  }
+
+  return start;
+}
+
 function onArrayLoaded (arr) {
   console.log('loaded');
-
-  // arr.sort(function(a, b){
-  //   return a-b;
-  // });
-
-  // console.log('sorted');
-  // console.log(arr[0], arr[arr.length-1]);
 
   var hash = {};
   var arrClear = [];
@@ -26,7 +60,7 @@ function onArrayLoaded (arr) {
     if(hash[item] === true) {
     } else {
       hash[item] = true;
-      arrClear.push(item);
+      arrClear.push(~~item);
     }
   });
 
@@ -48,19 +82,32 @@ function onArrayLoaded (arr) {
       percent = i * 100 / total;
       console.log(percent, '%');
     }
-    // var y_min = -10000 - x;
-    // var y_max = 10000 - x;
 
-    // var y_min_i = findBiggerIndex(arrClear, y_min);
-    // var y_max_i = findSmallerIndex(arrClear, y_max);
+    var y_min = -10000 - x;
+    var y_max = 10000 - x;
+// console.log(x, y_min, y_max);
 
+    var y_min_i = findIndex(arrClear, y_min);
+// console.log('min yi', y_min_i);
+    var y_max_i = findIndex(arrClear, y_max);
+// console.log('max yi', y_max_i);
 
-    for(var j=-10000; j<=10000; j++) {
-      var y = j-x;
-      if(y != x && hash[y] === true) {
-        hashT[j] = true;
+    for(var y_i=y_min_i; y_i<=y_max_i; y_i++) {
+      var y = arrClear[y_i];
+      if(y != x ) {
+        var t = x+y;
+        if (t >= -10000 && t <= 10000) {
+          hashT[x+y] = true;
+        }
       }
     }
+
+    // for(var j=-10000; j<=10000; j++) {
+    //   var y = j-x;
+    //   if(y != x && hash[y] === true) {
+    //     hashT[j] = true;
+    //   }
+    // }
   });
 
   console.log('result:', Object.keys(hashT).length);
